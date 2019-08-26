34ede0c039654795bf40bef17cd38cf83003eeff
mahdi <mahdi.guesmi@tek-up.tn>
2019-08-26 20:24:52 +0100

    fixing Cors problem

--- src/main/java/org/sid/Services/CompteRestService.java
+++ src/main/java/org/sid/Services/CompteRestService.java
@@ -5,12 +5,8 @@ import java.util.List;
 import org.sid.Controller.CompteController;
 import org.sid.Entities.Compte;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class CompteRestService {
@@ -19,6 +15,8 @@ CompteController compteController;
 	
 	@RequestMapping(value="/comptesave",method=RequestMethod.POST)
 	@ResponseBody
+	@CrossOrigin
+
 	public Compte saveCompte(@RequestBody Compte cp) {
 		System.out.println(cp.toString());
 		return compteController.saveCompte(cp);
